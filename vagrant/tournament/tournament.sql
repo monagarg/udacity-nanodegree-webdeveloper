259bdcb97fdd972c343615fbabb0d62d48fcca0c
mgggg <garg.mona@gmail.com>
2015-11-11 18:09:52 -0800

    create database and tables

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,13 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Create the tournament database
+CREATE DATABASE tournament;
 
+CREATE TABLE  players (ID serial primary key, name text);
+
+CREATE TABLE matches (ID serial references players(ID), win int, match int);
+
+insert into players (name) values ('mona');
+
+select count(ID) from players;
