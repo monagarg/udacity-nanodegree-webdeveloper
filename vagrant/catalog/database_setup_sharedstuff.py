212d609aa43b85b542b8674a41a01a8fb6a75bc4
mgggg <garg.mona@gmail.com>
2015-12-24 17:26:14 -0800

    updated code where oauth connect and disconnect are  working

--- vagrant/catalog/database_setup_sharedstuff.py
+++ vagrant/catalog/database_setup_sharedstuff.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -11,29 +11,29 @@ class User(Base):
     __tablename__ = 'user'
    
     user_id = Column(Integer, primary_key=True)
-    email_id = Column(String(250), nullable=True)
-    name = Column(String(250), nullable=False)
+    email_id = Column(String(250), nullable=False)
+    name = Column(String(250), nullable=True)
 
 class Product(Base):
     __tablename__ = 'product'
 
     product_id = Column(Integer, primary_key = True)
     name = Column(String(250), nullable = False)
-    description = Column(String(250),nullable=False)
-    category = Column(String(250),nullable=False)
-    identifier = Column(String(250),nullable=False)
-    identifier_value = Column(String(250),nullable=False)
-    is_active = Column(String(2))
+    description = Column(String(250))
+    category = Column(String(250))
+    identifier = Column(String(250))
+    identifier_value = Column(String(250))
+    is_active = Column(Boolean, default=True, nullable=False)
 
 class Listing(Base):
     __tablename__ = 'listing'
 
     listing_id = Column(Integer, primary_key = True)
-    user_id = Column(Integer, ForeignKey('user.user_id'),nullable=True)
-    product_id = Column(Integer, ForeignKey('product.product_id'),nullable=True)
+    user_id = Column(Integer, ForeignKey('user.user_id'),nullable=False)
+    product_id = Column(Integer, ForeignKey('product.product_id'),nullable=False)
     deposit = Column(String(8))
     max_days = Column(Integer)
-    is_rented = Column(String(2))
+    is_available = Column(Boolean, default=True, nullable=False)
     user = relationship(User)
     product = relationship(Product)
 
